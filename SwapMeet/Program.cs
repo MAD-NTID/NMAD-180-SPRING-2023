ad4f0299defb3878cba123d318526bdecf89496e
Kemoy Campbell <kscics@rit.edu>
2023-02-15 15:45:50 -0500

    mock exam1 solution

---
+++ SwapMeet/Program.cs
@@ -0,0 +1,61 @@
+﻿//setting up constants
+const string BUSINESS_NAME = "Disks4Cheap";
+const double DISK_PRICE = 25.75;
+
+Console.WriteLine(BUSINESS_NAME);
+
+//ask for the inventory
+Console.Write("Enter current inventory:");
+
+//validate that the inventory is a number and positive
+if(!int.TryParse(Console.ReadLine(), out int inventory) || inventory < 0){
+    Console.WriteLine("Please enter a positive number!");
+    System.Environment.Exit(0);
+}
+
+//show the user the menu
+Console.WriteLine("Menu\n========");
+Console.WriteLine("1. Display current inventory value");
+Console.WriteLine($"2. Buy disks from {BUSINESS_NAME}");
+Console.WriteLine($"3. Sell disks to {BUSINESS_NAME}");
+Console.WriteLine("4. Exit");
+
+//prompt the user what they want to do from the menu
+Console.Write("Selection:");
+switch(Console.ReadLine())
+{
+    case "1":
+        double totalCost = inventory * DISK_PRICE;
+        Console.WriteLine($"Total inventory:{inventory} and Total cost:{totalCost:C}");
+        break;
+    case "2":
+        Console.Write("How many disks do you want to sell:");
+        int sold = int.Parse(Console.ReadLine());
+        if(sold > inventory){
+            Console.WriteLine("disks total must not be more than the inventory disks!");
+        } else {
+            inventory-=sold;
+            totalCost = inventory * DISK_PRICE;
+            Console.WriteLine($"Total inventory:{inventory} and Total cost:{totalCost:C}");
+        }
+        break;
+    case "3":
+        
+        Console.Write("How many disks are you buying:");
+        int buying = int.Parse(Console.ReadLine());
+        if( buying < 0) {
+            Console.WriteLine("Please enter a positive number!");
+        } else {
+            inventory+=buying;
+            totalCost = inventory * DISK_PRICE;
+            Console.WriteLine($"Total inventory:{inventory} and Total cost:{totalCost:C}");
+        }
+        break;
+    case "4":
+        Console.WriteLine("Thank you for visiting us");
+        break;
+
+    default:
+        Console.WriteLine("Invalid selection!");
+        break;
+}
---
+++ WingsHeaven/Program.cs
@@ -0,0 +1,124 @@
+﻿/**
+Name: Kemoy Campbell
+Description: This is a program that performs wings of heaven sale receipts
+Date:2/15/2023
+**/
+
+const string BUSINESS_NAME = "Wings Heaven";
+
+//prices constants
+const double BONE_LESS = 7.99;
+const double TRADITION = 9.99;
+const double BOTH_WINGS = 17.98;
+
+const double NY_TAX = 0.08;
+const double WINGS_DISCOUNT = .18;
+
+const double BUFFALO_SAUCE = 0.29;
+const double MILD_SAUCE = 0.39;
+const double BBQ_SAUCE = 0.29;
+const double PLAIN_SAUCE = 0;
+
+const double FRIES = 3.49;
+const double RINGS = 2.49;
+const double BOTH_SIDES = 5.98;
+const double NO_SIDES = 0;
+
+const string OPTION_ONE = "1";
+const string OPTION_TWO = "2";
+const string OPTION_THREE = "3";
+const string OPTION_FOUR = "4";
+
+double  total = 0;
+string order = "";
+
+Console.WriteLine("Type of Wings:");
+Console.WriteLine($"\t1.Boneless {BONE_LESS:C}");
+Console.WriteLine($"\t2. Tradational {TRADITION:C}");
+Console.WriteLine($"\t3. Both {BOTH_WINGS:C}");
+
+Console.Write("Selection:");
+
+switch(Console.ReadLine())
+{
+    case OPTION_ONE:
+        total+=BONE_LESS;
+        order+=$"Boneless Wing {BONE_LESS:C}\n";
+        break;
+    case OPTION_TWO:
+        total+=TRADITION;
+        order+=$"Traditional Wings {TRADITION:C}\n";
+        break;
+    case OPTION_THREE:
+        double discount = BOTH_WINGS * WINGS_DISCOUNT;
+        total+= BOTH_WINGS - discount;
+        order+=$"Both wings {BOTH_WINGS:C}\nDiscount:{discount:C}\n";
+        break;
+
+
+}
+
+Console.WriteLine("Type of Sauces");
+Console.WriteLine($"\t1.Buffalo {BUFFALO_SAUCE:C}");
+Console.WriteLine($"\t2.Mild {MILD_SAUCE:C}");
+Console.WriteLine($"\t3.BBQ {BBQ_SAUCE:C}");
+Console.WriteLine($"\tPlain {PLAIN_SAUCE:C}");
+
+Console.Write("Selection:");
+switch(Console.ReadLine())
+{
+    case OPTION_ONE:
+        order+=$"Buffalo Sauce {BUFFALO_SAUCE:C}\n";
+        total+=BUFFALO_SAUCE;
+        break;
+    case OPTION_TWO:
+        order+=$"Mild Sauce {MILD_SAUCE:C}\n";
+        total+=MILD_SAUCE;
+        break;
+    case OPTION_THREE:
+        order+=$"BBQ Sauce {BBQ_SAUCE:C}\n";
+        total+=BBQ_SAUCE;
+        break;
+    
+    case OPTION_FOUR:
+        order+=$"Plain No Charge";
+        break;
+}
+
+
+//sides
+Console.WriteLine("Type of Sides");
+Console.WriteLine($"\t1.Waffle Fries {FRIES:C}");
+Console.WriteLine($"\t2.Onion Rings {RINGS:C}");
+Console.WriteLine($"\t3.Both {BOTH_SIDES:C}");
+Console.WriteLine($"\tNone {NO_SIDES:C}");
+
+Console.Write("Selection:");
+switch(Console.ReadLine())
+{
+    case OPTION_ONE:
+        //Console.WriteLine($"Waffle Fries {FRIES:C}"); -- you can also do this
+        order+=$"Waffle Fries {FRIES:C}\n";
+        total+=FRIES;
+        break;
+    case OPTION_TWO:
+        order+=$"Onion Rings {RINGS:C}\n";
+        total+=RINGS;
+        break;
+    case OPTION_THREE:
+        order+=$"Both Sides {BOTH_SIDES:C}\n";
+        total+=BOTH_SIDES;
+        break;
+    
+    case OPTION_FOUR:
+        order+=$"No Sides No Charge";
+        break;
+}
+Console.WriteLine();
+Console.WriteLine(BUSINESS_NAME);
+Console.WriteLine(order);
+Console.WriteLine($"Subtotal:{total:C}");
+double tax = total * NY_TAX;
+Console.WriteLine($"NY State Tax:{tax:C}");
+total+=tax;
+Console.WriteLine($"Total:{total:C}");
