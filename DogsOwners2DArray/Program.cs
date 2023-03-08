50a314a977343f4332875eb851fd3e5c7ba8afd5
Michael Berrios <mxbics@rit.edu>
2023-03-08 15:51:19 -0500

    In class demo for Method3.0 (overload methods, and optional params, and 2D Array

---
+++ DogsOwners2DArray/Program.cs
@@ -0,0 +1,168 @@
+﻿const int ROWS = 50;
+const int COLS = 3;
+
+//  This is how you declare and initialize a 2D array
+string[,] dogsOwners = new string[ROWS, COLS];
+
+//  This is how you initialize an array with values
+dogsOwners[0, 0] = "Floffy";
+dogsOwners[0, 1] = "Michael";
+dogsOwners[0, 2] = "Mike's Sister";
+
+dogsOwners[1, 0] = "Chewy";
+dogsOwners[1, 1] = "Eric";
+dogsOwners[1, 2] = "Eric's Sister";
+
+//  This is how you get the counts for rows
+// Console.WriteLine($"Row Counts: {dogsOwners.GetLength(0)}");
+
+//  This is how you get the counts for columns
+// Console.WriteLine($"Columns Counts: {dogsOwners.GetLength(1)}");
+
+string[] menu = {"Add Dogs and Owners", "Show Dogs and Owners", "Remove a Dog and Owners", "Exit"};
+bool continueMenu = true;
+
+while(continueMenu)
+{
+    switch(ShowMenu())
+    {
+        case "1":
+            AddDogAndOwners();
+            break;
+        case "2":
+            ShowDogsAndOwners();
+            break;
+        case "3":
+            RemoveADogAndOwners();
+            break;
+        case "4":
+            continueMenu = false;
+            break;
+
+        default:
+            Console.WriteLine($"Invalid Menu Option. Please choose between 1 and {menu.Length}");
+            break;
+    }
+
+    Console.WriteLine();
+}
+
+Console.WriteLine("Thank you for using the Dog and Owners Registration");
+
+string ShowMenu()
+{
+    for(int i = 0; i < menu.Length; i++)
+    {
+        Console.WriteLine($"{i + 1}: {menu[i]}");
+    }
+
+    Console.Write($"Enter an Option Between 1 and {menu.Length}: ");
+
+    return Console.ReadLine();
+}
+
+void AddDogAndOwners()
+{
+    string dogName = string.Empty;
+
+    while(string.IsNullOrEmpty(dogName))
+    {
+        Console.Write($"Enter the dog's name: ");
+        dogName = Console.ReadLine();
+
+        if(string.IsNullOrEmpty(dogName))
+            Console.WriteLine("Invalid Dog's Name");
+    }
+
+    //  We need to add the dog to the 2D array where the row is empty
+    int rowWhereDogIsInserted = 0;
+    for(int row = 0; row < dogsOwners.GetLength(0); row++)
+    {
+        if(string.IsNullOrEmpty(dogsOwners[row,0]))   
+        {
+            dogsOwners[row,0] = dogName;
+
+            //  We need the row to insert the owners in the correct row for the dog
+            rowWhereDogIsInserted = row;
+            break;
+        }
+    }
+
+    //  Now we add the owners
+    for(int col = 1; col < dogsOwners.GetLength(1); col++)
+    {
+        while(true)
+        {
+            Console.Write($"Enter Owner #{col}'s Name: ");
+            string ownerName = Console.ReadLine();
+
+            if(!string.IsNullOrEmpty(ownerName))
+            {
+                dogsOwners[rowWhereDogIsInserted, col] = ownerName;   
+                break;
+            }
+            else
+                Console.WriteLine("Invalid Dog Name");
+        }
+    }
+}
+
+void RemoveADogAndOwners()
+{
+    Console.Write("Enter the dog's name to remove: ");
+    string dogName = Console.ReadLine();
+    bool removed = false;
+    
+    for(int row = 0; row < dogsOwners.GetLength(0); row++)
+    {
+        // Console.WriteLine(dogsOwners[row, 0]);
+
+        //  Alternatively, you can do things like these:
+        // if(dogsOwners[row, 0].Equals(dogName, StringComparison.CurrentCultureIgnoreCase)) { }
+
+        //  This check is important because we have null values in the array
+        if(!string.IsNullOrEmpty(dogsOwners[row,0]) && dogsOwners[row,0].ToLower() == dogName.ToLower())   
+        {
+            //  Alternatively, you can do things like these:
+            //  dogsOwners[row, 0] = string.Empty;
+            dogsOwners[row, 0] = "";
+
+            //  Remove the Owners
+            for(int col = 0; col < dogsOwners.GetLength(1); col++)
+            {
+                dogsOwners[row, col] = string.Empty;
+            }
+
+            //  This is a flag for when a dog is removed, so we can show a message
+            removed = true;
+        }
+    }
+
+    if(removed)
+        Console.WriteLine($"{dogName} was removed");
+    else
+        Console.WriteLine($"{dogName} was not found");
+}
+
+void ShowDogsAndOwners()
+{
+    for (int row = 0; row < dogsOwners.GetLength(0); row++)
+    {
+        if(!string.IsNullOrEmpty(dogsOwners[row, 0]))
+        {
+            for (int col = 0; col < dogsOwners.GetLength(1); col++)
+            {
+                if (col == 0 && !string.IsNullOrEmpty(dogsOwners[row, col]))
+                {
+                    Console.WriteLine($"The dog's name is {dogsOwners[row, col]}, and its owners are: ");
+                    continue;
+                }
+
+                if (!string.IsNullOrEmpty(dogsOwners[row, col]))
+                    Console.WriteLine($"Dog Owner #{col}: {dogsOwners[row, col]}");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Method3.0/Program.cs
@@ -0,0 +1,99 @@
+﻿class MethodThreePointO
+{
+    static void Main(string[] args)
+    {
+        string[] menuChoices = {"Add Student", "Add Student with Age", "Add Student with Age and Major", "Exit"};
+
+        Console.WriteLine("This program doesn't actually add any students to an array.\n" + 
+        "It's simply to show, how you can use overload methods");
+
+        while(true)
+        {
+            string studentName, major;
+            int age;
+
+            switch(ShowMenu(menuChoices))
+            {
+                case "1":
+                    Console.Write("Enter Student's Name:");
+                    studentName = Console.ReadLine();
+
+                    AddStudent(studentName);
+                    break;
+
+                case "2":
+                    Console.Write("Enter Student's Name:");
+                    studentName = Console.ReadLine();
+
+                    Console.Write("Enter Student's Age:");
+                    age = int.Parse(Console.ReadLine());
+
+                    AddStudent(studentName, age);
+                    break;
+
+                case "3":
+                    Console.Write("Enter Student's Name:");
+                    studentName = Console.ReadLine();
+
+                    Console.Write("Enter Student's Age:");
+                    age = int.Parse(Console.ReadLine());
+
+                    Console.Write("Add Major (y/n)? ");
+                    string majorACT = Console.ReadLine();
+
+                    if(majorACT.ToLower() == "n")
+                        AddStudent(studentName, age);
+                    else
+                    {
+                        Console.Write("What is the student's major? ");
+                        major = Console.ReadLine();
+                        AddStudent(studentName, age, major);
+                    }
+                    
+                    break;
+
+                case "4":
+                    Console.WriteLine("Bye bye now");
+                    Environment.Exit(0);
+
+                    break;
+
+                default: 
+                    Console.WriteLine("Invalid Choice");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+
+        //  Alternatively
+        // string choice = ShowMenu(menuChoices);
+
+        // switch(choice)
+        // {
+
+        // }
+    }
+
+    static string ShowMenu(string[] menuChoices) 
+    {
+        for(int i = 0; i < menuChoices.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}: {menuChoices[i]}");
+        }
+
+        Console.Write("Select an Option: ");
+
+        return Console.ReadLine();
+    }
+
+    static void AddStudent(string name)
+    {
+        Console.WriteLine($"Student Name is: {name}");
+    }
+
+    static void AddStudent(string name, int age, string major = "ACT")
+    {
+        Console.WriteLine($"Student Name is: {name} and their age is {age}. The student's major is {major}.");
+    }
+}
