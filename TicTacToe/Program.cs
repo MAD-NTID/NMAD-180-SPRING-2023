10cb83f4129ea472a27760ba22a44b8e6f613201
Michael Berrios <mxbics@rit.edu>
2023-03-10 17:17:50 -0500

    Improved TicTacToe. Simpler logics for diagonal wins. Added Tie check

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,58 +1,82 @@
-﻿const int ROWS = 3;
+﻿const int WIN_COUNT = 3;
+const int ROWS = 3;
 const int COLS = 3;
 
 const string X = "X";
 const string O = "O";
 
+// This is how you declare the 2D if playing live by inputs
 //string[,] board = new string[ROWS,COLS];
 
-//  Row Win for X
+// Tie, no body Wins!
+string[,] board = {
+    {X, X, O},
+    {O, O, X},
+    {X, O, O}
+};
+
+// Row #1 Win for X [0,0], [0,1], [0,2]
 // string[,] board = {
 //     {X, X, X},
 //     {O, O, X},
 //     {X, O, O}
 // };
-//  0,0 
-//  0,1
-//  0,2
 
+// Row #2 Win for X [1,0], [1,1], [1,2]
+// string[,] board = {
+//     {X, O, O},
+//     {X, X, X},
+//     {O, X, O}
+// };
+
+// Row #3 Win for X [2,0], [2,1], [2,2]
+// string[,] board = {
+//     {X, O, O},
+//     {O, O, X},
+//     {X, X, X}
+// };
+
+// Column #1 Win for X [0,0], [1,0], [2,0]
 // string[,] board = {
 //     {X, O, X},
 //     {X, O, O},
 //     {X, X, O}
 // };
-//  0,0
-//  1,0
-//  2,0
 
+// Column #2 Win for X [0,1], [1,1], [2,1]
+// string[,] board = {
+//     {O, X, O},
+//     {O, X, X},
+//     {X, X, O}
+// };
 
+// Column #3 Win for X [0,2], [1,2], [2,2]
+// string[,] board = {
+//     {O, X, X},
+//     {O, O, X},
+//     {X, O, X}
+// };
+
+// Diagonal Left to Right Win for X [0,0], [1,1], [2,2]
 // string[,] board = {
 //     {X, O, X},
 //     {O, X, O},
-//     {X, O, X}
+//     {O, O, X}
 // };
-//  Diagonal Win from left to right
-//  0,0
-//  1,1
-//  2,2
 
-string[,] board = {
-    {O, O, X},
-    {O, X, X},
-    {X, O, O}
-};
-//  Diagonal Win from right to left
-//  0,2
-//  1,1
-//  2,0
+// Diagonal Right to Left Win for X [0,2], [1,1], [2,0]
+// string[,] board = {
+//     {O, O, X},
+//     {O, X, X},
+//     {X, O, O}
+// };
 
-int xCount = 0;
-int oCount = 0;
-
+//  Display the Game Board
 for(int row = 0; row < ROWS; row++)
 {
     for(int col = 0; col < COLS; col++)
     {
+        //  Prints the value for the Coordinate row, col
         Console.Write($"{board[row,col]} ");
     }
 
@@ -60,12 +84,21 @@ for(int row = 0; row < ROWS; row++)
     Console.WriteLine();
 }
 
+//  These variables to be used by all game logic
+int xCount = 0;
+int oCount = 0;
+//  This flag is used to determine whether to continue the game or not. 
+//  Additionally, this flag prevents further checking of game logic in case there are two winners of the same symbol.
+//  Finally, this flag is used to show if a Tie has happened.
+bool gameIsWon = false;
+
 //  Check for Rows Winning
 for(int row = 0; row < ROWS; row++)
 {
+    //  We clear the xCount and oCount if a row does not contain any 3 consecutive symbols
+    //  This is so we can proceed to the next row and test again
     xCount = 0;
     oCount = 0;
-
 
     for(int col = 0; col < COLS; col++)
     {
@@ -74,23 +107,24 @@ for(int row = 0; row < ROWS; row++)
             xCount++;
         else
             oCount++;
-
-        //  Check if there is a win
-        //  Check if X won
-        if(xCount == 3)
-        {
-            Console.WriteLine("X Won on a Row");
-            break;
-        }
-        
-        //  Check if O won
-        else if(oCount == 3)
-        {
-            Console.WriteLine("O Won on a Row");
-            break;
-        }
-
     }
+
+    //  Check if X won
+    if(xCount == WIN_COUNT)
+    {
+        Console.WriteLine("X Won on a Row");
+        gameIsWon = true;
+    }
+    
+    //  Check if O won
+    else if(oCount == WIN_COUNT)
+    {
+        Console.WriteLine("O Won on a Row");
+        gameIsWon = true;
+    }
+
+    if(gameIsWon)
+        Environment.Exit(0);
 }
 
 //  Check for Column Winning
@@ -105,80 +139,75 @@ for(int col = 0; col < COLS; col++)
             xCount++;
         else
             oCount++;
-
-        //  Check if X Won
-        if(xCount == 3)
-        {
-            Console.WriteLine("Player X Won on a Column");
-            break;
-        }
-        //  Check if O Won
-        else if(oCount == 3)
-        {
-            Console.WriteLine("Player O Won on a Column");
-        }
     }
+
+    if(xCount == WIN_COUNT)
+    {
+        Console.WriteLine("Player X Won on a Column");
+        gameIsWon = true;
+    }
+    else if(oCount == WIN_COUNT)
+    {
+        Console.WriteLine("Player O Won on a Column");
+        gameIsWon = true;
+    }
+
+    if(gameIsWon)
+        Environment.Exit(0);
 }
 
 //  Check for Diagonal Winning left to right
 xCount = 0; 
 oCount = 0;
+
 for(int row = 0; row < ROWS; row++)
 {
-    for(int col = 0; col < COLS; col++)
-    {
-        //  Only if Row and Col are equal (this means diagonal)
-        if(row == col)
-        {
-            if(board[row, col] == X)
-                xCount++;
-            else if(board[row, col] == O)
-                oCount++;
-        }
+    if(board[row, row] == X)
+        xCount++;
+    else if(board[row, row] == O)
+        oCount++;
 
-        //  Now we check for the winner
-        if(xCount == 3)
-        {
-            Console.WriteLine("X Won Diagonally from left to right");
-            break;
-        }
-        else if(oCount == 3)
-        {
-            Console.WriteLine("O Won Diagonally from left to right");
-            break;
-        }
+    if(xCount == WIN_COUNT)
+    {
+        Console.WriteLine("X Won Diagonally from Left to Right");
+        gameIsWon = true;   
     }
+    else if(oCount == WIN_COUNT)
+    {
+        Console.WriteLine("O Won Diagonally from Left to Right");
+        gameIsWon = true;
+    }
+
+    if(gameIsWon)
+        Environment.Exit(0);
 }
 
-//  Check for Diagonal Winning right to left
-int row1 = 0;   // Start from row 0
-int col1 = COLS -1; // Start from bottom-most column 2
+//  Check for Diagonal Winning Right to Left
 xCount = 0; 
 oCount = 0;
 
-while(col1 >= 0 && row1 < ROWS)
+for(int row = 0; row < ROWS; row++)
 {
-    if(board[row1, col1] == X)
+    //  Start from right most column. As the row number increases, the row is substracted from the column. 
+    //  This makes the column moves to the left
+    if(board[row, COLS - row - 1] == X)
         xCount++;
-    else
+    else if(board[row, COLS - row - 1] == O)
         oCount++;
 
-    if(xCount == 3)
+    if(xCount == WIN_COUNT)
     {
         Console.WriteLine("X Won Diagonally from Right to Left");
-        break;
+        gameIsWon = true;
     }
-    else if(oCount == 3)
+    else if(oCount == WIN_COUNT)
     {
         Console.WriteLine("O Won Diagonally from Right to Left");
-        break;
+        gameIsWon = true;
     }
 
-    //  Decrease the column
-    col1--;
-
-    //  Increment the row
-    row1++;
-
-    //  THe above causes the column to move to the left while at the same time the row is moving up
+    if(gameIsWon)
+        Environment.Exit(0);
 }
+
+Console.WriteLine("Tie, no body won");
