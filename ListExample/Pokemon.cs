0c5dc363176802583e2e846da349756fea3d1d3b
Kemoy Campbell <kscics@rit.edu>
2023-04-10 14:56:01 -0400

    list example

---
+++ ListExample/Pokemon.cs
@@ -0,0 +1,82 @@
+public class Pokemon
+{
+    private string name;
+    public string Name
+    {
+        get { return name; }
+        set 
+        { 
+            if(string.IsNullOrEmpty(value))
+                value = "N/A";
+
+            name = value; 
+        }
+    }
+
+    private string  color;
+    public string  Color
+    {
+        get { return color; }
+        set 
+        { 
+            if(string.IsNullOrEmpty(value))
+                value = "N/A";
+            color = value; 
+        }
+    }
+
+    private int level;
+    public int Level
+    {
+        get { return level; }
+        set 
+        { 
+            if(value < 1)
+                value = 1;
+
+            level = value; 
+        }
+    }
+
+
+
+    private string  owner;
+    public string  Owner
+    {
+        get { return owner; }
+        set 
+        { 
+            if(string.IsNullOrEmpty(value))
+                value = "N/A";
+            owner = value; 
+        }
+    }
+
+    //default constructor
+    public Pokemon()
+    {
+        Name = "N/A";
+        Level = 1;
+        Owner = "N/A";
+        Color = "N/A";
+    }
+
+    public Pokemon(string name, string color, string owner, int level)
+    {
+        Name = name;
+        Color = color;
+        Owner = owner;
+        Level = level;
+    }
+
+    public override string ToString()
+    {
+        return $"Name:{name} Level:{level} Color:{color} Owner:{owner}";
+    }
+    
+
+
+    
+    
+    
+}
---
+++ ListExample/Program.cs
@@ -0,0 +1,62 @@
+﻿// List<string> names = new List<string>();
+
+// string[] namesArray = new string[2];
+
+// //adding an element in the list. The element will be added at the end
+// names.Add("Erik");
+
+// //adding an element to the begining of the list, all other elements will be shifted to the right
+// names.Insert(0, "Kemoy");
+
+// Console.WriteLine($"The name at index 0 is :{names[0]}");
+// Console.WriteLine($"The name at index 1 is :{names[1]}");
+
+// //adding an element at the same index location in an array will replace the existing value
+// namesArray[0] = "Erik";
+// namesArray[0] = "Kemoy";
+
+// Console.WriteLine($"The name at index 0 is :{namesArray[0]}");
+
+// //removing an element from the list will reduce the size
+// names.Remove("Erik");
+// names.RemoveAt(0);
+
+// Console.WriteLine(names.Count);
+
+// List<string> items = new List<string>();
+// items.Add("Osman");
+// items.Add("Erik");
+// Console.WriteLine("Calling list before clear");
+// ShowInfo(items);
+
+// //show elements using foreach
+// foreach(string item in items)
+// {
+//     Console.WriteLine(item);
+// }
+
+// Console.WriteLine("Calling list after clear");
+// items.Clear();
+// ShowInfo(items);
+
+
+
+
+// void ShowInfo(List<string> stuffs)
+// {
+//     for(int i = 0; i < stuffs.Count; i++)
+//         Console.WriteLine($"index:{i} value:{stuffs[i]}");
+// }
+
+
+List<Pokemon> pokemons = new List<Pokemon>();
+Pokemon pokemon = new Pokemon("Pikchau", "Yellow", "Ash", 100);
+pokemons.Add(pokemon);
+
+
+foreach(Pokemon pok in pokemons)
+{
+    if(pok.Name == "Pikchau"){
+        pokemons.Remove(pok);
+    }
+}
